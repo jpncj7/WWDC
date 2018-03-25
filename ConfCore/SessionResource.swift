24c50654588d284d732864997ab8d1eb14a5fd8c
Ben Newcombe <bcmn96@gmail.com>
2018-03-25 19:02:46 +0100

    Rebase + wiring up of adapters. Storage.swift session assoc still needed

---
+++ ConfCore/SessionResource.swift
@@ -0,0 +1,25 @@
+//
+//  SessionResource.swift
+//  ConfCore
+//
+//  Created by Ben Newcombe on 09/01/2018.
+//  Copyright © 2018 Guilherme Rambo. All rights reserved.
+//
+
+import RealmSwift
+
+public enum SessionResourceType: String {
+    case none
+    case resource = "WWDCSessionResourceTypeResource"
+    case activity = "WWDCSessionResourceTypeActivity"
+}
+
+public class SessionResource: Object {
+
+    @objc public var identifier: String = ""
+    public var type: SessionResourceType = .none
+
+    public override class func primaryKey() -> String? {
+        return "identifier"
+    }
+}
